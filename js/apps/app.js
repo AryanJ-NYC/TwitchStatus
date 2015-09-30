a69be6edddcedb3cf4a006285e03cb3dc775ba84
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-09-30 01:09:52 -0400

    Finished fetching data from Twitch API

--- js/apps/app.js
+++ js/apps/app.js
@@ -1,27 +1,38 @@
 var twitchApp = angular.module('TwitchApp', []);
 
 twitchApp.controller('MainController', ['$scope', '$http', '$q', function ($scope, $http, $q) {
-    var baseStreamUrl = "https://api.twitch.tv/kraken/streams/";
-    var baseChannelUrl = "https://api.twitch.tv/kraken/channels/";
-    
-    var channels = ["freecodecamp",
+    var baseStreamUrl = "https://api.twitch.tv/kraken/streams/",
+        baseChannelUrl = "https://api.twitch.tv/kraken/channels/",
+        channels = ["freecodecamp",
                     "storbeck",
                     "terakilobyte",
                     "habathcx",
                     "RobotCaleb",
                     "thomasballinger",
                     "noobs2ninjas",
-                    "beohoff"];
-
+                    "beohoff"],
+        streamPromises = [],
+        channelPromises = [],
+        i;
     $scope.data = {};
-    promises = [];
     for (i = 0; i < channels.length; i++) {
-        promises.push($http.get(baseStreamUrl + channels[i]));
+        streamPromises.push($http.get(baseStreamUrl + channels[i]));
+        channelPromises.push($http.get(baseChannelUrl + channels[i]));
     }
-    $q.all(promises).then(function (array) {
+    $q.all(streamPromises).then(function (array) {
         for (i = 0; i < array.length; i++) {
-            channelName = channels[i];
-            $scope.data[channelName] = array[i].data;
+            var channelName = channels[i];
+            $scope.data[channelName] = {};
+            $scope.data[channelName].stream = array[i].data.stream;
+        }
+    });
+    $q.all(channelPromises).then(function (channelData) {
+        for (i = 0; i < channelData.length; i++) {
+            var channelName = channels[i];
+
+            $scope.data[channelName].displayName = channelData[i].data.display_name;
+            $scope.data[channelName].logo = channelData[i].data.logo;
+            $scope.data[channelName].url = channelData[i].data.url;
         }
     });
 }]);
