a1148632993250fe416d41a19d5cda32c3ce61f0
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-09-30 20:17:14 -0400

    All channels showing on page

--- js/apps/app.js
+++ js/apps/app.js
@@ -19,6 +19,7 @@ twitchApp.controller('MainController', ['$scope', '$http', '$q', function ($scop
         streamPromises.push($http.get(baseStreamUrl + channels[i]));
         channelPromises.push($http.get(baseChannelUrl + channels[i]));
     }
+    
     $q.all(streamPromises).then(function (array) {
         for (i = 0; i < array.length; i++) {
             var channelName = channels[i];
@@ -26,12 +27,17 @@ twitchApp.controller('MainController', ['$scope', '$http', '$q', function ($scop
             $scope.data[channelName].stream = array[i].data.stream;
         }
     });
+    
     $q.all(channelPromises).then(function (channelData) {
         for (i = 0; i < channelData.length; i++) {
             var channelName = channels[i];
 
             $scope.data[channelName].displayName = channelData[i].data.display_name;
-            $scope.data[channelName].logo = channelData[i].data.logo;
+            if (channelData[i].data.logo) {
+                $scope.data[channelName].logo = channelData[i].data.logo;
+            } else {
+                $scope.data[channelName].logo = "http://placehold.it/300";
+            }
             $scope.data[channelName].url = channelData[i].data.url;
         }
     });
